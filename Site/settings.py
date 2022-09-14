62551b11eb901ceb554985a1f6a43426c8ed09a0
vbello-tech <odunayobello1@gmail.com>
2022-09-14 03:36:25 +0100

    front

--- Site/settings.py
+++ Site/settings.py
@@ -141,8 +141,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'Static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 CRISPY_TEMPLATE_PACK ="bootstrap4"
