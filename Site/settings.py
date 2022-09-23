d08ebd02352c3ece08e80f3b29376d566aa2b75d
vbello-tech <odunayobello1@gmail.com>
2022-09-23 10:52:55 +0100

    role

--- Site/settings.py
+++ Site/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
